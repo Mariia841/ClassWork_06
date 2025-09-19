ff846314bb090e41d79c0ed983ab3cf56236dd79
dev <dev@A21PC8>
2025-09-19 17:44:06 +0300

    Add project files.

---
+++ ClassWork_06/MusicDbPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork_06
+{
+    internal class MusicDbPlayer
+    {
+
+        public MusicDbPlayer()
+        {
+        
+        }
+
+        
+    }
+
+    class Artists
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sruname { get; set; }
+        public string Country { get; set; }
+        
+
+    }
+
+    class Albums
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int ArtistId { get; set; }
+    }
+    class Tracks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public int AlbumId { get; set; }
+    }
+
+    class Playlists
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TracksId { get; set; }
+
+    }
+
+    class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+}
---
+++ ClassWork_06/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassWork_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicDbPlayer musicDbPlayer = new MusicDbPlayer();
+
+            
+        }
+    }
+}
